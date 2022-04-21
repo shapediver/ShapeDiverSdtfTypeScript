4e2662db5bec3016906d9e16b4c43f9bcc4cd031
Matthias Reisacher <matt@shapediver.com>
2022-04-21 18:01:03 +0200

    Fix bug in writeable buffer formatter

--- packages/sdk.sdtf-v1/src/utils/SdDtfUtils.ts
+++ packages/sdk.sdtf-v1/src/utils/SdDtfUtils.ts
@@ -5,12 +5,12 @@ export function userComponentToDataObject (o: object): Record<string, unknown> {
     const additionalProperties = Object.keys(o).filter(prop => prop === "additionalProperties")
     const dataObject: Record<string, unknown> = (isDataObject(additionalProperties)) ? additionalProperties : {}
     Object.entries(o)
-        .filter(([ key, _ ]) => key !== "componentId" && key !== "additionalProperties")
+        .filter(([ key, _ ]) => key !== "componentId" && key !== "additionalProperties" && key !== "data")
         .forEach(([ key, value ]) => dataObject[key] = value)
     return dataObject
 }
 
 /** Creates a random component id. */
-export function createComponentId(): string {
+export function createComponentId (): string {
     return Math.random().toString(36).substring(2, 9)
 }
