2365def86ef4c5cc41a07df0d3c334453268484c
Matthias Reisacher <matt@shapediver.com>
2022-05-10 17:25:10 +0200

    Update npm version in check-script

--- scripts/check-npm-version.sh
+++ scripts/check-npm-version.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 VERSION=$(npm -v)
-if [ $VERSION = '6.14.5' ]
-then
+if [ $VERSION = '6.14.15' ]; then
     exit 0
 else
-    echo 'You need to switch to a different version (6.14.5, node 14.5.0) of npm to continue.'
+    echo 'You need to switch to a different version (6.14.15, node 14.5.0) of npm to continue.'
     exit 1
 fi
